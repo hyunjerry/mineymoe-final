9f5cbb01c8f60c385dd9057e12d102063bdc20f4
hyunjerry <hyunjerry@hotmail.com>
2018-03-31 15:28:37 -0700

    fixed googl login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -9,7 +9,7 @@ OmniAuth.config.logger = Rails.logger
 # end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '116762353372-e5eqaqousip5c8dbfj3eh715pavs5jjf.apps.googleusercontent.com', 'KCnOkGR2AdH2j8KziYudlgBA', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '659394411369-uir2671khso2odigai6ds9aojoihnd3h.apps.googleusercontent.com', 'Tr7_dW_J86j2OSOkB83Codj6', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
 
 
