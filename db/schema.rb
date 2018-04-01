9e0214930be75120d7f4fed4afb987d70daa68f9
David <davidkim0228@gmail.com>
2018-04-01 03:26:02 -0700

    button

--- db/schema.rb
+++ db/schema.rb
@@ -38,12 +38,6 @@ ActiveRecord::Schema.define(version: 20180401050620) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "tmpdatabasaes", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "provider"
     t.string "uid"
